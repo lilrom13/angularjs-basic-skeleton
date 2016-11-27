de8759cf10100e9f99d805666ec09dc3c1547a5a
Margheriti <lilrom13@gmail.com>
2016-11-27 17:10:43 -0500

    minimalize

--- app/app.js
+++ app/app.js
@@ -3,11 +3,22 @@
  */
 'use strict';
 
-// Declare app level module which depends on views, and components
-var app = angular.module('myApp', ['ngRoute', 'myApp.home']).
+
+var home = angular.module('myApp.home', ['ngRoute']).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('!');
 
-    $routeProvider.otherwise({redirectTo: '/templates/home/home.html'});
+    $routeProvider
+        .when('/', {templateUrl: 'templates/home/home.html',
+            controller: 'homeController'})
+        .otherwise({redirectTo: '/templates/home/home.html'});
+}]);
+
+angular.module('myApp', ['ngRoute', 'myApp.home']).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
+
+    $routeProvider
+    .otherwise({redirectTo: '/templates/home/home.html'});
 }]);
 
